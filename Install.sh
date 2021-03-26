0c3dc4660c5bac2c57ac7280f79c11072dccd30d
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2021-03-26 14:29:19 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,13 +1,13 @@
 cd $HOME
 clear
 echo "                 Installing..."
-pkg update
-pkg upgrade
+pkg update -y
+pkg upgrade -y
 clear
 echo "Пакеты Termux обновлены, идёт установка следующих программ: fakeroot, python, python2, python3, pip, openssh."
-pkg install fakeroot
-pkg install python2
-pkg install python3
-pkg install openssh
+pkg install fakeroot -y
+pkg install python -y
+pkg install python3 -y
+pkg install openssh -y
 clear
 echo "Установлено успешно!"
