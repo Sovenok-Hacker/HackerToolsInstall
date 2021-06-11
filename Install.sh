9c7f30d7d1a29b919d643575c6996a3d2fdf4f4e
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2021-06-11 20:24:33 +0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -4,10 +4,19 @@ echo "                 Installing..."
 pkg update -y
 pkg upgrade -y
 clear
-echo "Пакеты Termux обновлены, идёт установка следующих программ: fakeroot, python, python2, python3, pip, openssh."
+echo "Пакеты Termux обновлены, идёт установка пакета!"
 pkg install fakeroot -y
 pkg install python -y
 pkg install python3 -y
 pkg install openssh -y
+pkg install openssl -y
+pkg install termux-tools -y
+pkg install vim nano wget curl -y
+pip install pyTelegramBotAPI
+pip install fuzzywuzzy
+pip install aiogram
+pip install Pillow
+pip install requests
+pip install discord.py
 clear
 echo "Установлено успешно!"
