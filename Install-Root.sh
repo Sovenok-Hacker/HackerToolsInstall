cce23f652036d0d50e962c636e8b726a1f7ca7f7
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2021-06-11 20:31:54 +0300

    Create Install-Root.sh

---
+++ Install-Root.sh
@@ -0,0 +1,7 @@
+clear
+pkg install tsu -y
+pkg install unstable-repo -y
+pkg install root-repo -y
+pkg install curl wget nano vim python python3 ruby cython clang make docker apt apt-get pkg termux-tools openssh openssl -y
+echo Утилиты установлены!
+echo Exitting...
