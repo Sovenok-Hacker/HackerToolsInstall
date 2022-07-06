c58f53eb35b90e56149fa878c2ea21cb100a8826
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-07-06 17:18:32 +0300

    Update site.py

--- site.py
+++ site.py
@@ -2,4 +2,4 @@ import os
 print('Хотите ли вы открыть мой сайт? 1 - да, 2 - нет')
 text = input()
 if text == '1':
-               os.system('termux-open https://sovenok-hacker.ml')
+  os.system('termux-open https://sovenok-hacker.ml')
